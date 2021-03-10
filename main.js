9e5fb6bc9a3447b2815f84a9329b651c874557f8
Amarinhobest <tama1611201187@gmail.com>
2021-03-10 13:37:15 +0200

    Firsr commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const min = require('min').min;
+const abs = require('abs').abs;
+const summ = require('summ').summ;
+
+let array1=[13, 2744, 3456, 9];
+
+min(array1);
+
+console.log('');
+
+summ(13, 274, 3456, 9);
+
+console.log('');
+
+abs(-17);
